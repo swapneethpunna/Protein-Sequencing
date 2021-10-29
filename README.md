ef2b7e7607f6580fd837780564a6f6ec4bc85a10
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2021-10-29 21:04:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Social-Media-Analytics
+
+
+hw6_social.py - The skeleton code of the Protein Sequencing is ready in this python file.
+hw6_social_tests.py - The testing of the Protein Sequencing is in this python file.
---
+++ hw6_protein.py
@@ -0,0 +1,208 @@
+"""
+Protein Sequencing Project
+Name:
+Roll Number:
+"""
+
+import hw6_protein_tests as test
+
+project = "Protein" # don't edit this
+
+### WEEK 1 ###
+
+'''
+readFile(filename)
+#1 [Check6-1]
+Parameters: str
+Returns: str
+'''
+def readFile(filename):
+    return
+
+
+'''
+dnaToRna(dna, startIndex)
+#2 [Check6-1]
+Parameters: str ; int
+Returns: list of strs
+'''
+def dnaToRna(dna, startIndex):
+    return
+
+
+'''
+makeCodonDictionary(filename)
+#3 [Check6-1]
+Parameters: str
+Returns: dict mapping strs to strs
+'''
+def makeCodonDictionary(filename):
+    import json
+    return
+
+
+'''
+generateProtein(codons, codonD)
+#4 [Check6-1]
+Parameters: list of strs ; dict mapping strs to strs
+Returns: list of strs
+'''
+def generateProtein(codons, codonD):
+    return
+
+
+'''
+synthesizeProteins(dnaFilename, codonFilename)
+#5 [Check6-1]
+Parameters: str ; str
+Returns: 2D list of strs
+'''
+def synthesizeProteins(dnaFilename, codonFilename):
+    return
+
+
+def runWeek1():
+    print("Human DNA")
+    humanProteins = synthesizeProteins("data/human_p53.txt", "data/codon_table.json")
+    print("Elephant DNA")
+    elephantProteins = synthesizeProteins("data/elephant_p53.txt", "data/codon_table.json")
+
+
+### WEEK 2 ###
+
+'''
+commonProteins(proteinList1, proteinList2)
+#1 [Check6-2]
+Parameters: 2D list of strs ; 2D list of strs
+Returns: 2D list of strs
+'''
+def commonProteins(proteinList1, proteinList2):
+    return
+
+
+'''
+combineProteins(proteinList)
+#2 [Check6-2]
+Parameters: 2D list of strs
+Returns: list of strs
+'''
+def combineProteins(proteinList):
+    return
+
+
+'''
+aminoAcidDictionary(aaList)
+#3 [Check6-2]
+Parameters: list of strs
+Returns: dict mapping strs to ints
+'''
+def aminoAcidDictionary(aaList):
+    return
+
+
+'''
+findAminoAcidDifferences(proteinList1, proteinList2, cutoff)
+#4 [Check6-2]
+Parameters: 2D list of strs ; 2D list of strs ; float
+Returns: 2D list of values
+'''
+def findAminoAcidDifferences(proteinList1, proteinList2, cutoff):
+    return
+
+
+'''
+displayTextResults(commonalities, differences)
+#5 [Check6-2]
+Parameters: 2D list of strs ; 2D list of values
+Returns: None
+'''
+def displayTextResults(commonalities, differences):
+    return
+
+
+def runWeek2():
+    humanProteins = synthesizeProteins("data/human_p53.txt", "data/codon_table.json")
+    elephantProteins = synthesizeProteins("data/elephant_p53.txt", "data/codon_table.json")
+
+    commonalities = commonProteins(humanProteins, elephantProteins)
+    differences = findAminoAcidDifferences(humanProteins, elephantProteins, 0.005)
+    displayTextResults(commonalities, differences)
+
+
+### WEEK 3 ###
+
+'''
+makeAminoAcidLabels(proteinList1, proteinList2)
+#2 [Hw6]
+Parameters: 2D list of strs ; 2D list of strs
+Returns: list of strs
+'''
+def makeAminoAcidLabels(proteinList1, proteinList2):
+    return
+
+
+'''
+setupChartData(labels, proteinList)
+#3 [Hw6]
+Parameters: list of strs ; 2D list of strs
+Returns: list of floats
+'''
+def setupChartData(labels, proteinList):
+    return
+
+
+'''
+createChart(xLabels, freqList1, label1, freqList2, label2, edgeList=None)
+#4 [Hw6] & #5 [Hw6]
+Parameters: list of strs ; list of floats ; str ; list of floats ; str ; [optional] list of strs
+Returns: None
+'''
+def createChart(xLabels, freqList1, label1, freqList2, label2, edgeList=None):
+    import matplotlib.pyplot as plt
+    return
+
+
+'''
+makeEdgeList(labels, biggestDiffs)
+#5 [Hw6]
+Parameters: list of strs ; 2D list of values
+Returns: list of strs
+'''
+def makeEdgeList(labels, biggestDiffs):
+    return
+
+
+'''
+runFullProgram()
+#6 [Hw6]
+Parameters: no parameters
+Returns: None
+'''
+def runFullProgram():
+    return
+
+
+### RUN CODE ###
+
+# This code runs the test cases to check your work
+if __name__ == "__main__":
+    print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
+    test.week1Tests()
+    print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
+    runWeek1()
+
+    ## Uncomment these for Week 2 ##
+    """
+    print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
+    test.week2Tests()
+    print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
+    runWeek2()
+    """
+
+    ## Uncomment these for Week 3 ##
+    """
+    print("\n" + "#"*15 + " WEEK 3 TESTS " +  "#" * 16 + "\n")
+    test.week3Tests()
+    print("\n" + "#"*15 + " WEEK 3 OUTPUT " + "#" * 15 + "\n")
+    runFullProgram()
+    """
