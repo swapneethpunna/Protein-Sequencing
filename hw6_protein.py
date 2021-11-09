86aedca124d7838032b598ca77924734eff743dc
swapneethpunna <swapneethpunna@msitprogram.net>
2021-11-09 14:29:16 +0530

    Generate Amino Acid Dictionary

--- hw6_protein.py
+++ hw6_protein.py
@@ -150,7 +150,13 @@ Parameters: list of strs
 Returns: dict mapping strs to ints
 '''
 def aminoAcidDictionary(aaList):
-    return
+    dict={}
+    for i in aaList:
+        if i not in dict:
+            dict[i]=1
+        else:
+            dict[i]+=1
+    return dict
 
 
 '''
@@ -243,7 +249,7 @@ if __name__ == "__main__":
     # test.week1Tests()
     # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     # runWeek1()
-    test.testCombineProteins()
+    test.testAminoAcidDictionary()
 
     ## Uncomment these for Week 2 ##
     """
